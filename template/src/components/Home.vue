746ad97da1bea5b4ea6a1045760057fc0616f843
alexandre <aobellianne@phoceis.asia>
2017-07-19 18:25:07 +0800

    Add needed packages. Modify home page

---
+++ template/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="content">
+    <h1>Default content</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+
+</style>
